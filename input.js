00e54dabcc6e14a1f54abea64ece5be6516351f7
Samuel Davis <samueljakdavis@gmail.com>
2017-09-24 11:00:27 -0400

    tap map edge to move

--- input.js
+++ input.js
@@ -1,60 +1,27 @@
 class InputHandler {
-    constructor(map, controls, canvas) {
+    constructor(map, screen) {
         this.map = map;
-        this.canvas = canvas;
-        for (let key in InputHandler.CONTROLS) {
-            if (!InputHandler.CONTROLS.hasOwnProperty(key)) {
-                continue;
-            }
-            const label = document.createElement('label');
-            label.setAttribute('for', `#${key}`);
-            label.innerText = key.replace('_', ' ');
-            const input = document.createElement('input');
-            input.setAttribute('type', 'text');
-            input.setAttribute('id', key);
-            input.setAttribute('label', key);
-            input.setAttribute('value', InputHandler.CONTROLS[key]);
-            input.addEventListener('keyup', ({target, key}) => {
-                if (Object.values(InputHandler.CONTROLS).includes(key)) {
-                    target.value = target.getAttribute('value');
-                } else {
-                    target.setAttribute('value', key);
-                    target.value = key;
-                    InputHandler.CONTROLS[target.getAttribute('id')] = key;
-                }
-            });
-            controls.appendChild(input);
-            controls.appendChild(label);
-        }
+        screen.elem.addEventListener('click', ({target, offsetX, offsetY}) => {
+            const styles = window.getComputedStyle(target);
+            const x = parseInt(styles.width.replace('px', '')) / 2;
+            const y = parseInt(styles.height.replace('px', '')) / 2;
+            const deltaX = x - offsetX;
+            const deltaY = y - offsetY;
 
-        this.bindKeyboard();
-        this.bindMobile();
-    }
+            console.log(x, offsetX, deltaX);
 
-    bindMobile() {
-        this.canvas.addEventListener('click', ({target, offsetX, offsetY}) => {
-            const deltaX = Math.abs(target.width / 2 - offsetX);
-            const deltaY = Math.abs(target.height / 2 - offsetY);
-            if (deltaX > deltaY) {
-                if (offsetX < target.width / 2) {
+            if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                if (deltaX > 0) {
                     this.handleEvent(InputHandler.CONTROLS.MOVE_WEST);
                 } else {
                     this.handleEvent(InputHandler.CONTROLS.MOVE_EAST);
                 }
             } else {
-                if (offsetY < target.height / 2) {
+                if (deltaY > 0) {
                     this.handleEvent(InputHandler.CONTROLS.MOVE_NORTH);
                 } else {
                     this.handleEvent(InputHandler.CONTROLS.MOVE_SOUTH);
                 }
-            }
-        });
-    }
-
-    bindKeyboard() {
-        document.addEventListener('keyup', ({target, key}) => {
-            if (!Object.keys(InputHandler.CONTROLS).includes(target.getAttribute('id'))) {
-                this.handleEvent(key);
             }
         });
     }
